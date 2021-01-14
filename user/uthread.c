35aca47fece20eee2cc4c3c227005090a30b9611
yuchen <yl6448@nyu.edu>
2021-01-13 17:33:35 -0800

    first

--- user/uthread.c
+++ user/uthread.c
@@ -10,16 +10,35 @@
 #define STACK_SIZE  8192
 #define MAX_THREAD  4
 
+struct context {
+  uint64 ra;
+  uint64 sp;
+
+  // callee-saved
+  uint64 s0;
+  uint64 s1;
+  uint64 s2;
+  uint64 s3;
+  uint64 s4;
+  uint64 s5;
+  uint64 s6;
+  uint64 s7;
+  uint64 s8;
+  uint64 s9;
+  uint64 s10;
+  uint64 s11;
+};
 
 struct thread {
   char       stack[STACK_SIZE]; /* the thread's stack */
   int        state;             /* FREE, RUNNING, RUNNABLE */
-
+  struct context context;
 };
 struct thread all_thread[MAX_THREAD];
 struct thread *current_thread;
 extern void thread_switch(uint64, uint64);
-              
+void thread_yield(void);
+
 void 
 thread_init(void)
 {
@@ -63,6 +82,7 @@ thread_schedule(void)
      * Invoke thread_switch to switch from t to next_thread:
      * thread_switch(??, ??);
      */
+    thread_switch((uint64)&t->context, (uint64)&current_thread->context);
   } else
     next_thread = 0;
 }
@@ -76,7 +96,10 @@ thread_create(void (*func)())
     if (t->state == FREE) break;
   }
   t->state = RUNNABLE;
-  // YOUR CODE HERE
+  t->context = current_thread->context;
+  t->context.ra = (uint64) func;
+  t->context.sp = (uint64) t->stack + 4096;
+  thread_yield();
 }
 
 void 
@@ -134,6 +157,7 @@ thread_c(void)
 {
   int i;
   printf("thread_c started\n");
+
   c_started = 1;
   while(a_started == 0 || b_started == 0)
     thread_yield();
@@ -159,5 +183,6 @@ main(int argc, char *argv[])
   thread_create(thread_b);
   thread_create(thread_c);
   thread_schedule();
+
   exit(0);
 }
